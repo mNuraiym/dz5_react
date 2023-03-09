1721a97d864a725316a23fefd3dfa39db79267fc
mNuraiym <nuraika180903@gmail.com>
2023-03-09 17:10:54 +0600

    dz5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(){
+
+    const { register, handleSubmit } = useForm()
+
+    const onSubmit = (data) => console.log(data);
+
+
+    return(
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("Login", {required:true, maxLength: 15})}/>
+            <input type="password" {...register("Password", {required:true, minLength: 4})}/>
+            <input type="submit"/>
+        </form>
+    )
 }
 
-export default App;
+export default App
